d94f8dd16cdbb17d5bc3b7f32a5be7938d5dc520
CY-Zhang <czhang376@wisc.edu>
2021-09-21 12:08:40 -0400

    Started new BOtorch version.

--- Ronchigram_simulation/misc/px_to_ang.m
+++
@@ -1,8 +0,0 @@
-function ret = px_to_ang(simdim,kev)
-if ~exist('kev','var')
-      kev = 300;
-end
-lambda = 12.3986./sqrt((2*511.0+kev).*kev) * 10^-10;
-ret = lambda*1/(2*simdim/1000)*10^10; 
-
-end
--- Ronchigram_simulation/simulation/aperture_mask.m
+++
@@ -1,7 +0,0 @@
-function [mask] = aperture_mask(imdim,simdim,ap_size)
-    al_max = simdim; 
-    al_vec = (linspace(-al_max,al_max,imdim));
-    [alxx,alyy] = meshgrid(al_vec,al_vec);
-    al_rr = sqrt(alxx.^2 + alyy.^2);
-    mask = al_rr<= ap_size;
-end
